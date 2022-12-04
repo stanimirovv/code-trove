2a604a1494789e6855bcc356c8b5b77d4e017748
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-12-04 19:01:47 +0200

    feat: day 4

---
+++ advent-of-code-2022/day4/day4.ts
@@ -0,0 +1,56 @@
+import fs from "fs";
+
+function getTotalScore(path: string) {
+  const cleaningSectionPairs: CleaningSection[][] = readEntriesFromFile(path);
+  const overLappingElvesCount = cleaningSectionPairs.filter(
+    ([sectionA, sectionB]) => doSectionsOverlap(sectionA, sectionB)
+  ).length;
+  return overLappingElvesCount;
+}
+
+type CleaningSection = {
+  start: number;
+  end: number;
+};
+
+function readEntriesFromFile(path: string) {
+  try {
+    return fs
+      .readFileSync(path)
+      .toString()
+      .split("\n")
+      .filter((line) => line !== undefined)
+      .map((line) => {
+        const [elfA, elfB] = line.split(",");
+        const [startA, endA] = elfA.split("-");
+        const [startB, endB] = elfB.split("-");
+        const cleaningSectionsA: CleaningSection = {
+          start: Number.parseInt(startA),
+          end: Number.parseInt(endA),
+        };
+        const cleaningSectionsB: CleaningSection = {
+          start: Number.parseInt(startB),
+          end: Number.parseInt(endB),
+        };
+        return [cleaningSectionsA, cleaningSectionsB];
+      });
+  } catch (err: any) {
+    console.log("Cloud not read input file: ", err.toString);
+    throw err;
+  }
+}
+
+function doSectionsOverlap(
+  sectionA: CleaningSection,
+  sectionB: CleaningSection
+): boolean {
+  const aOverlapsB =
+    sectionA.start <= sectionB.start && sectionA.end >= sectionB.end;
+  const bOverlapsA =
+    sectionB.start <= sectionA.start && sectionB.end >= sectionA.end;
+
+  return aOverlapsB || bOverlapsA;
+}
+
+console.log(getTotalScore("./input_test"));
+console.log(getTotalScore("./input"));
